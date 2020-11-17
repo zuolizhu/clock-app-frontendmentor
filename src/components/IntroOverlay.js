338671b6eea42e2a8caa45dbf907520d42db5bbf
zuolizhu <zuolizhu5960x@gmail.com>
2020-11-17 14:39:14 -0500

    update loading animation

--- src/components/IntroOverlay.js
+++ src/components/IntroOverlay.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function IntroOverlay({ overlayRef }) {
   return (
     <div ref={overlayRef} className="intro-overlay">
-      <p><span class="glitch">loading</span></p>
+      <div className="jawn"></div>
+      <p className="t-loading">loading</p>
     </div>
   )
 }
